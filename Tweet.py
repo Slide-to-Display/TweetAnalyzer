c9246814b76ed5f259e805b6e26ea02f824d100b
zhul6 <zhulitie@gmail.com>
2014-10-29 19:15:16 -0400

    finished analyzer

---
+++ Tweet.py
@@ -0,0 +1,12 @@
+class Tweet:
+	def __init__(self, _tweet, _geo, _sentiment = ""):
+		self.tweet = _tweet.strip()
+		self.geo = _geo.strip()
+		self.sentiment = _sentiment
+
+	def __str__(self):
+		return ("text: " + self.tweet + " | sentiment: " + self.sentiment)
+
+	def __repr__(self):
+		return ("text: " + self.tweet + " | sentiment: " + self.sentiment)
+
